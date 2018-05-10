c3b4da4bfb2725c030bf291a9eb55e216aa90fa8
Nikita Hovratov <nikita.h@live.de>
2018-05-10 15:26:17 +0200

    Use WebSocket

--- bin/chat-server.php
+++ bin/chat-server.php
@@ -1,11 +1,17 @@
 <?php
 
 use MyApp\Chat;
+use Ratchet\Http\HttpServer;
 use Ratchet\Server\IoServer;
+use Ratchet\WebSocket\WsServer;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 $server = IoServer::factory(
-    new Chat(),
+    new HttpServer(
+        new WsServer(
+            new Chat()
+        )
+    ),
     8080
 );
 
