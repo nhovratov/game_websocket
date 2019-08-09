08f9b486da301b5d0e6932261c8eeae7d3d1b9d7
Nikita Hovratov <nikita.h@live.de>
2019-08-09 21:27:24 +0200

    Update phpunit to version 8 and fix things

--- tests/Unit/LoveLetter/LoveLetterTest.php
+++ tests/Unit/LoveLetter/LoveLetterTest.php
@@ -19,7 +19,7 @@ class LoveLetterTest extends TestCase
 {
     protected $mockStackProvider;
 
-    public function setUp()
+    public function setUp(): void
     {
         $this->mockStackProvider = new MockStackProvider();
     }
